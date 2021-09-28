fa9d725395ae7d73de6397437c38a3ce55c0d318
OsAdminLuke <thecookielegends@gmail.com>
2021-09-27 19:38:52 -0500

    gain and fall exponential

--- src/vena_drum.cpp
+++ src/vena_drum.cpp
@@ -42,7 +42,7 @@ struct Vena_drum : Module {
 		}
 
 		if (rising) {
-			volume += params[ATTACK_PARAM].getValue() / 500;
+			volume += pow(params[ATTACK_PARAM].getValue(),2) / 1000;
 			volume = clamp(volume, 0.f, 1.f);
 			if (volume == 1) {
 				rising = false;
@@ -50,7 +50,7 @@ struct Vena_drum : Module {
 		}
 
 		if (!rising) {
-			volume -= params[DECAY_PARAM].getValue() / 500;
+			volume -= pow(params[DECAY_PARAM].getValue(),2) / 1000;
 			volume = clamp(volume, 0.f, 1.f);
 		}
 
